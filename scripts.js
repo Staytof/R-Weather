5cf4991e0cffeb6f0f550107b9087c818b0a8ffa
Gabriel S. Dias <102713815+Staytof@users.noreply.github.com>
2023-02-05 00:41:34 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ let chave = "cebcd482eda57fa9a6714c1c2ba91885"
 function colocarNaTela(dados){
     console.log(dados)
     document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity
-    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".cidade").innerHTML = "Tempo em: " + dados.name
     document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C"
     document.querySelector(".descricao").innerHTML = dados.weather[0].description
     document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
